4ef49d7385cfc393a76d0a0a8faaea8d6543ca4a
xukele <giliguala1024@qq.com>
2022-07-09 22:26:13 +0800

    git init 11

--- service/src/main/java/com/example/hello_java_native/delegate/KeleApiImpl.java
+++ service/src/main/java/com/example/hello_java_native/delegate/KeleApiImpl.java
@@ -4,19 +4,19 @@ import com.example.hello_java_native.gen.api.KeleApiDelegate;
 import com.example.hello_java_native.gen.model.KeleResult;
 import com.example.hello_java_native.gen.model.UserInfoData;
 import com.example.hello_java_native.gen.model.UserInfoRsp;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KeleApiImpl implements KeleApiDelegate {
 
+    @Value("${kele.value}")
+    Integer age;
+
     @Override
     public ResponseEntity<UserInfoRsp> queryUserInfoById(Long id) {
-        return ResponseEntity.ok(
-                new UserInfoRsp()
-                .ret(new KeleResult().code(1).message("ok"))
-                .data(new UserInfoData().username("xukele").age(11))
-        );
+        return ResponseEntity.ok(new UserInfoRsp().ret(new KeleResult().code(1).message("ok")).data(new UserInfoData().username("xukele").age(age)));
     }
 
 

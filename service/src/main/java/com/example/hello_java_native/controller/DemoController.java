614602d8bbfb1155fb90ad6c2561b4d203042248
xukele <giliguala1024@qq.com>
2022-07-09 16:53:18 +0800

    git init9

--- service/src/main/java/com/example/hello_java_native/controller/DemoController.java
+++ service/src/main/java/com/example/hello_java_native/controller/DemoController.java
@@ -1,9 +1,9 @@
 package com.example.hello_java_native.controller;
 
-import com.example.hello_java_native.api.KeleApiDelegate;
-import com.example.hello_java_native.model.KeleResult;
-import com.example.hello_java_native.model.UserInfoData;
-import com.example.hello_java_native.model.UserInfoRsp;
+import com.example.hello_java_native.gen.api.KeleApiDelegate;
+import com.example.hello_java_native.gen.model.KeleResult;
+import com.example.hello_java_native.gen.model.UserInfoData;
+import com.example.hello_java_native.gen.model.UserInfoRsp;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +12,12 @@ public class DemoController implements KeleApiDelegate {
 
     @Override
     public ResponseEntity<UserInfoRsp> queryUserInfoById(Long id) {
-        return ResponseEntity.ok(new UserInfoRsp()
+        return ResponseEntity.ok(
+                new UserInfoRsp()
                 .ret(new KeleResult().code(1).message("ok"))
-                .data(new UserInfoData().username("xukele").age(11)));
+                .data(new UserInfoData().username("xukele").age(11))
+        );
     }
+
+
 }

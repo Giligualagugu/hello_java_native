0870f4ef9223073d80964308b5d41f454712479b
xukele <giliguala1024@qq.com>
2022-07-24 11:04:54 +0800

    user swagger codegen

--- service/src/main/java/com/example/hello_java_native/delegate/KeleApiImpl.java
+++ service/src/main/java/com/example/hello_java_native/delegate/KeleApiImpl.java
@@ -4,16 +4,13 @@ import com.example.hello_java_native.bridge.ComputeService;
 import com.example.hello_java_native.gen.api.KeleApiDelegate;
 import com.example.hello_java_native.gen.api.TimeApiDelegate;
 import com.example.hello_java_native.gen.model.KeleResult;
-import com.example.hello_java_native.gen.model.TestBridge200Response;
+import com.example.hello_java_native.gen.model.SaveUserInfoRsp;
 import com.example.hello_java_native.gen.model.UserInfoData;
 import com.example.hello_java_native.gen.model.UserInfoRsp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.NativeWebRequest;
-
-import java.util.Optional;
 
 @Component
 public class KeleApiImpl implements KeleApiDelegate, TimeApiDelegate {
@@ -23,22 +20,24 @@ public class KeleApiImpl implements KeleApiDelegate, TimeApiDelegate {
 
     // webrequest 属于运行时创建的proxy对象,不支持 native-image
 
-    @Override
-    public Optional<NativeWebRequest> getRequest() {
-        return KeleApiDelegate.super.getRequest();
-    }
+
 
     @Override
     public ResponseEntity<UserInfoRsp> queryUserInfoById(Long id) {
         return ResponseEntity.ok(new UserInfoRsp().ret(new KeleResult().code(1).message("ok")).data(new UserInfoData().username("xukele").age(age)));
     }
 
+    @Override
+    public ResponseEntity<SaveUserInfoRsp> saveUserInfo(UserInfoData body) {
+        return null;
+    }
+
     @Autowired
     ComputeService computeService;
 
     @Override
-    public ResponseEntity<TestBridge200Response> testBridge(Long uid, String appId) {
+    public ResponseEntity<Object> testBridge(Long uid, String appId) {
         Integer andCompute = computeService.getAndCompute(uid, appId);
-        return ResponseEntity.ok(new TestBridge200Response().code(0).time(andCompute));
+        return ResponseEntity.ok(andCompute);
     }
 }
